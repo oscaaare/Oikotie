6aef225cd3182f15199bb7a779360b58bf13444b
oscaaare <oscar.rahtu@gmail.com>
2020-05-22 01:29:57 +0300

    tokaversio

--- oikotie.py
+++ oikotie.py
@@ -2,81 +2,172 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import pandas as pd
+from datetime import datetime
 
 t0 = time.time()
 
-house_id = "15712871" #"15711635"
-house_id_stop = 15732880 #15711650
-house_row_data = []
-final_data = ''
-data = []
-houses = []
-data_rows = 0
-total_rows_parsed = 0
-i = 0
+fname = 'Oikotie_houses_for_sale'+datetime.now().strftime("%d_%m_%Y")+'.xlsx'
+house_id = "15713743"# 5252117 pitäisi olla ensimmäinen
+house_id_stop = 15713760 #15716055 pitäisi olla viimeinen
 delimiter = '€'
-
 headers = {
         "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'}
 
+
 # Looping through the html pages and collecting the data.
-while int(house_id) < house_id_stop:
-    URL = "https://asunnot.oikotie.fi/myytavat-asunnot/helsinki/" + house_id
+def collect_data(headers_def, house_id_def, house_id_stop_def, delimiter_def):
+    house_row_data = []
+    empty_house_ids_def = []
+    not_sale_house_ids_def = []
+    final_data = ''
+    data_def = []
+    help_title_list = []
+    data_rows = 0
+    total_rows_parsed_def = 0
+    i = 0
+    data_rows_parsed_def = 0
+    tasaluku = int(house_id_def)
 
-    page = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(page.content, 'lxml')
+    while int(house_id_def) < house_id_stop_def:
+        url = "https://asunnot.oikotie.fi/myytavat-asunnot/helsinki/" + house_id_def
 
-    house_sale_notice = soup.find_all('span', {'class': 'listing-breadcrumbs__item'})
+        page = requests.get(url, headers=headers_def)
+        soup = BeautifulSoup(page.content, 'lxml')
+        message = ''
 
-    if house_sale_notice:
-        if house_sale_notice[0].get_text() == 'Myytävät asunnot':
-            info_table_row = soup.find_all('div', {'class': 'info-table__row'})
-            if info_table_row:
-                while i < len(soup.find_all('dd', {'class': 'info-table__value'})):
+        house_sale_notice = soup.find_all('span', {'class': 'listing-breadcrumbs__item'})
 
-                    if i == 0:
-                        house_row_data.append(house_id)
+        if house_sale_notice:
+            if house_sale_notice[0].get_text() == 'Myytävät asunnot':
+                info_table_row = soup.find_all('div', {'class': 'info-table__row'})
+                if info_table_row:
+                    while i < len(soup.find_all('dd', {'class': 'info-table__value'})):
+                        if i == 0:
+                            house_row_data.append(house_id_def)
+                            data_rows_parsed_def += 1
 
-                    #print(info_table_row[i].get_text())
-                    #print(info_table_row[i].findChildren("dt")[0].get_text()) #tiedon title
-                    # print(info_table_row[i].findChildren("dd")[0].get_text()) #itse tieto
+                        info_table_title = info_table_row[i].findChildren("dt")
+                        info_table_value = info_table_row[i].findChildren("dd")
 
-                    info_table_title = info_table_row[i].findChildren("dt")
+                        if info_table_title:
 
-                    if info_table_title:
-                        if info_table_title[0].get_text() == 'Sijainti':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].findChildren("a")[1].get_text())) #postikoodi
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].findChildren("a")[2].get_text())) #kaupunki
+                            info_table_title_text = info_table_title[0].get_text()
 
-                        if info_table_row[i].findChildren("dt")[0].get_text() == 'Kaupunginosa':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].findChildren("a")[0].get_text())) #kaupunginosa
+                            if info_table_title_text == 'Sijainti':
+                                house_row_data.append(
+                                    str(info_table_value[0].findChildren("a")[1].get_text())[:5]) #postikoodi
+                                help_title_list.append(0)
 
-                        if info_table_row[i].findChildren("dt")[0].get_text() == 'Asuinpinta-ala':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].get_text()).replace(' m²', '')) #asuinpinta-ala
+                            if info_table_title_text == 'Sijainti':
+                                house_row_data.append(
+                                    str(info_table_value[0].findChildren("a")[2].get_text()))  # kaupunki
+                                help_title_list.append(1)
 
-                        if info_table_row[i].findChildren("dt")[0].get_text() == 'Huoneita':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].get_text())) #huoneita
+                            if info_table_title_text == 'Kaupunginosa':
+                                house_row_data.append(str(info_table_value[0].get_text())) #Kaupunginosa
+                                help_title_list.append(2)
 
-                        if info_table_row[i].findChildren("dt")[0].get_text() == 'Kunto':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].get_text())) #kunto
+                            if info_table_title_text == 'Asuinpinta-ala':
+                                house_row_data.append(
+                                    str(info_table_value[0].get_text()).replace(
+                                        ' m²', '').replace(
+                                        ',', '.')) #asuinpinta-ala
+                                help_title_list.append(3)
 
-                        if info_table_row[i].findChildren("dt")[0].get_text() == 'Myyntihinta':
-                            house_row_data.append(str(info_table_row[i].findChildren("dd")[0].get_text()).replace("€", '')) #Myyntihinta
+                            if info_table_title_text == 'Huoneita':
+                                house_row_data.append(str(info_table_value[0].get_text())) #huoneita
+                                help_title_list.append(4)
 
-                    i += 1
-                final_data = "€".join(house_row_data)
-                house_row_data.clear()
-                print(final_data)
-                data.append(final_data.split(delimiter))
-                i = 0
-                data_rows += 1
+                            if info_table_title_text == 'Kunto':
+                                house_row_data.append(str(info_table_value[0].get_text())) #kunto
+                                help_title_list.append(5)
 
-    house_id = str(int(house_id) + 1)
-    total_rows_parsed += 1
+                            if info_table_title_text == 'Myyntihinta':
+                                house_row_data.append(
+                                    str(info_table_value[0].get_text()).replace(
+                                        "€", '').replace(
+                                        '\xa0', '').replace(
+                                        ',', '.')) #Myyntihinta
+                                help_title_list.append(6)
+
+                        i += 1
+
+                    num = 0
+                    help_num = 0
+                    while num < len(help_title_list):
+                        if help_title_list[num] == help_num:
+                            help_num += 1
+                        else:
+                            help_num += 1
+                            house_row_data.insert(help_num, '')
+                            help_num += 1
+                        num += 1
+
+                    try:
+                        house_row_data.append(str(round(float(house_row_data[7]) / float(house_row_data[4]), 2))) #neliöhinta
+                    except:
+                        house_row_data.append(str('')) #neliöhinta, jos neliöt on nolla
+
+                    final_data = "€".join(house_row_data)
+                    house_row_data.clear()
+                    help_title_list.clear()
+                    print(final_data)
+                    data_def.append(final_data.split(delimiter_def))
+                    i = 0
+                    data_rows += 1
+            else:
+                not_sale_house_ids_def.append(house_id_def)
+                message = " - URL contains data, but it isn't a sale notice"
+        else:
+            empty_house_ids_def.append(house_id_def)
+            message = ' - Empty URL'
+
+        if message:
+            print(house_id_def + message)
+
+        house_id_def = str(int(house_id_def) + 1)
+        total_rows_parsed_def += 1
+
+    return data_def, total_rows_parsed_def, data_rows_parsed_def, empty_house_ids_def, not_sale_house_ids_def
+
+
+data, total_rows_parsed, data_rows_parsed, empty_house_ids, not_sale_house_ids = collect_data(headers, house_id, house_id_stop, delimiter)
+
+#Creating the DataFrame.
+df = pd.DataFrame(data, columns=['house_id', 'postal_code', 'city', 'city_part', 'square_meters', 'rooms', 'condition', 'sale_price', 'price_per_square_meter'])
+
+# Creating a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter(fname, engine='xlsxwriter', options={'strings_to_numbers': True})
+
+# Converting the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='Oikotie', index=False)
+
+#Creating cell formats using the xlsxwriter workbook and worksheet objects.
+workbook = writer.book
+worksheet = writer.sheets['Oikotie']
+format1 = workbook.add_format({'num_format': '#,##0.00'})
+format2 = workbook.add_format({'num_format': '00000'})
+
+#Applying formats to selected columns
+worksheet.set_column('A:A', 10, None)
+worksheet.set_column('B:B', 10, format2)
+worksheet.set_column('C:C', 10, None)
+worksheet.set_column('D:D', 10, None)
+worksheet.set_column('E:E', 10, format1)
+worksheet.set_column('F:F', 10, None)
+worksheet.set_column('G:G', 10, None)
+worksheet.set_column('H:H', 10, format1)
+worksheet.set_column('I:I', 10, format1)
+
+# Closing the Pandas Excel writer to output the Excel file.
+writer.save()
 
 print(data)
+print("Empty URLS", empty_house_ids)
+print("Not for sale URLS", not_sale_house_ids)
 t1 = time.time()
-total = t1-t0
-print("Parser in total", total_rows_parsed, "rows.")
-print("Time lapsed in total", total)
-print("Time for one URL", total/total_rows_parsed)
+total_time = t1-t0
+print("Parser in total", total_rows_parsed, "URLs.")
+print("Parser in total", data_rows_parsed, "URLs with data.")
+print("Time lapsed in total", total_time)
+print("Time for one URL", total_time/total_rows_parsed)
